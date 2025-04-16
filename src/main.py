179c2e51d63057d117d79826c474b8158b5d73b8
Joelxd <joel.cayllahua@utec.edu.pe>
2025-04-15 21:02:58 -0500

    feat: agregar operaciones de suma y multiplicación en la calculadora- Joel Cayllahua

--- src/main.py
+++ src/main.py
@@ -16,3 +16,17 @@ def calculate(entrada):
             raise ValueError("Entrada inválida")
     else:
         raise NotImplementedError("Solo se permite la suma por ahora")
+
+
+    if '*' in entrada:
+            partes = entrada.split('*')
+            if len(partes) != 2:
+                raise SyntaxError("Sintaxis inválida para la multiplicación")
+            try:
+                num1 = float(partes[0].strip())
+                num2 = float(partes[1].strip())
+                return num1 * num2
+            except ValueError:
+                raise ValueError("Entrada inválida")
+
+        raise NotImplementedError("Operación no implementada")
